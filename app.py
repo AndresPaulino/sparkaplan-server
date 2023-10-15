1b823af8564850bc1f50a3f93476940429042e98
Andres Paulino <paulinoandresmiguel@gmail.com>
2023-10-15 12:16:42 -0400

    Adjust cors value to accept all

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@localhost/{os.getenv('DB_NAME')}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_URI')}/{os.getenv('DB_NAME')}"
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     jwt = JWTManager(app)
     
     db.init_app(app)
     migrate = Migrate(app, db)
     
-    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3031"}}, supports_credentials=True)
+    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
     # Register blueprint within the create_app function
     app.register_blueprint(lesson_view, url_prefix='/api')
     app.register_blueprint(auth_view, url_prefix='/api')
     app.register_blueprint(user_view, url_prefix='/api')
     
-    
-    return app
+        
+        return app
 
-app = create_app()
+    app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
