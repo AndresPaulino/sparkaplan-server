c1c6e1e0e5e6d38d8159a2cae920a00fd84691a2
Andres Paulino <paulinoandresmiguel@gmail.com>
2023-10-15 13:33:24 -0400

    Correct foreign key for user_id

--- models/lesson.py
+++ models/lesson.py
@@ -5,13 +5,6 @@ from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 import uuid
 
-class User(db.Model):
-    __tablename__ = 'user'  # SQLAlchemy creates tables alphabetically by tablename
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    lessons = relationship('Lesson', backref='user', lazy=True)
-
 class Lesson(db.Model):
     __tablename__ = 'lesson'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -20,4 +13,4 @@ class Lesson(db.Model):
     learning_objective = db.Column(db.String(500), nullable=False)
     content = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)  # Ensure the table name matches User.__tablename__
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)  # Ensure the table name matches User.__tablename__
